90a550f8ee4bcfd7de681d66ec4ce894937d7244
Muthuraja997 <muthuraja05980@gmail.com>
2024-09-01 23:15:30 +0530

    updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,74 +1,65 @@
 
 import 'package:flutter/material.dart';
+import './navigate.dart';
 
 // import 'src/app.dart';
 // import 'src/settings/settings_controller.dart';
 // import 'src/settings/settings_service.dart';
-
 void main(){
-
   runApp(const MyApp());
-  // Set up the SettingsController, which will glue user settings to multiple
-  // Flutter Widgets.
-  //final settingsController = SettingsController(SettingsService());
-
-  // Load the user's preferred theme while the splash screen is displayed.
-  // This prevents a sudden theme change when the app is first displayed.
-  //await settingsController.loadSettings();
-
-  // Run the app and pass in the SettingsController. The app listens to the
-  // SettingsController for changes, then passes it further down to the
-  // SettingsView.
-  //runApp(MyApp(settingsController: settingsController));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
   
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-        title: const Text('Power cunsumption',
-        style: TextStyle(color: Color.fromARGB(223, 167, 245, 33)),),
-        actions: [
-          IconButton(onPressed: (){
-        },
-        icon: const Icon(Icons.notification_add)
-          ),
-        ],
-        backgroundColor: const Color.fromRGBO(255, 135, 245, 33),
-        ) ,
-        backgroundColor:const Color.fromARGB(223, 254, 243, 67) ,
-        drawer: Drawer(
-          child: Container(
-          color:const Color.fromARGB(224, 7, 222, 7),
-          child: ListView(
-            children: const [
-                  DrawerHeader(
-                    child: Center(
-                      child: Text('content1',style: TextStyle(fontSize: 33),
-                      ),
-                    ),
-                  ),
-                  ListTile(
-                    title: Text('page1'),
-                  ),
-                  ListTile(
-                    title: Text('page2'),
-                  ),
-            ],
-          ),
-          )
-        ),
-       ),
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: SafeArea(
+//         child: Scaffold(
+//           appBar: AppBar(
+//           title: const Text('Power cunsumption',
+//           style: TextStyle(color: Color.fromARGB(223, 167, 245, 33)),),
+//           actions: [
+//             IconButton(onPressed: (){
+//           },
+//           icon: const Icon(Icons.notification_add),
+//             ),
+//           ],
+//           backgroundColor: const Color.fromRGBO(255, 135, 245, 33),
+//           ) ,
+//           backgroundColor:const Color.fromARGB(223, 254, 243, 67) ,
+//           drawer: Drawer(
+//             child: Container(
+//             color:const Color.fromARGB(224, 7, 222, 7),
+//             child: ListView(
+//               children: const [
+//                     DrawerHeader(
+//                       child: Center(
+//                         child: Text('content1',style: TextStyle(fontSize: 33),
+//                         ),
+//                       ),
+//                     ),
+//                     ListTile(
+//                       title: Text('page1'),
+//                     ),
+//                     ListTile(
+//                       title: Text('page2'),
+//                     ),
+
+
+//               ],
+//             ),
+//             )
+//           ),
+//          ),
+//       ),
       
-      );
-}
-}
+//       );
+// }
+// }
 
 
 
