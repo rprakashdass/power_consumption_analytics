cafdf952c41c8a44bc8ffb789a5a80e698c005a4
Muthuraja997 <muthuraja05980@gmail.com>
2024-09-01 23:54:42 +0530

    ubdated

--- lib/navigate.dart
+++ lib/navigate.dart
@@ -18,8 +18,11 @@ class MyApp extends StatelessWidget {
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-          title: const Text('M-POWER',
-          style: TextStyle(color: Color.fromARGB(244, 237, 6, 6),fontSize:28),),
+            
+          title: const Center(
+            child: Text('M-POWER',textAlign: TextAlign.center,
+            style: TextStyle(color: Color.fromARGB(244, 237, 6, 6),fontSize:28),),
+          ),
           actions: [
             IconButton(onPressed: (){
           },
@@ -34,7 +37,7 @@ class MyApp extends StatelessWidget {
             color:const Color.fromARGB(224, 7, 222, 7),
               
             child: ListView(
-              children: const [
+              children: const <Widget>[
                     DrawerHeader(
                       child: Center(
                         child: Text('M-POWER',style: TextStyle(fontSize: 33),
@@ -42,24 +45,63 @@ class MyApp extends StatelessWidget {
                       ),
                     ),
                     
-                   Icon(Icons.account_circle, size: 30,applyTextScaling: true,semanticLabel:String.fromEnvironment('profile'),),
+                   Icon(Icons.account_circle, size: 30),
                   //  SizedBox(width: 10),
-                   Text(
-                      'Account', // Name of the icon
-                      style: TextStyle(fontSize: 25),
-                     ),
-                    ListTile(
-                      title: Text('page1'),
-                    ),
-                    ListTile(
-                      title: Text('page2'),
-                    ),
-                    ListTile(
-                      title: Text('page3'),
-                    ),
-                    ListTile(
-                      title: Text('page4'),
-                    ),
+                   Center(
+                     child: Text(
+                        'Account', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                   Icon(Icons.mobile_friendly, size: 30),
+                   Center(
+                     child: Text(
+                        'Device', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                    Icon(Icons.people),
+                    Center(
+                     child: Text(
+                        'User', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                    Icon(Icons.note_alt),
+                    Center(
+                     child: Text(
+                        'Report', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                   Icon(Icons.subscriptions),
+                   Center(
+                     child: Text(
+                        'Subscription', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                   Icon(Icons.notifications_active),
+                   Center(
+                     child: Text(
+                        'Notification settings', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                   Icon(Icons.lock_outline_rounded),
+                   Center(
+                     child: Text(
+                        'Change passward', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
+                   Icon(Icons.logout),
+                    Center(
+                     child: Text(
+                        'Logout', // Name of the icon
+                        style: TextStyle(fontSize: 25),
+                       ),
+                   ),
               ],
             ),
           
@@ -70,9 +112,7 @@ class MyApp extends StatelessWidget {
             BottomNavigationBarItem(label:'home', icon:Icon(Icons.home)),
             BottomNavigationBarItem(label:'thunder', icon:Icon(Icons.show_chart_outlined)),
           ],backgroundColor:const Color.fromARGB(122, 456, 234, 34),),
-          body: TextButton.icon(onPressed:(){
-      
-           } ,icon:const Icon(Icons.add), label:const Text('icon'))
+          
          ),
       ),
       );
