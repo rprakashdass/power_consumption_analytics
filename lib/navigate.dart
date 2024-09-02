6b66f82d4a5c089d1741e3ee76124322ca7a3e60
Muthuraja997 <muthuraja05980@gmail.com>
2024-09-02 23:19:56 +0530

    navigation ubdated

--- lib/navigate.dart
+++ lib/navigate.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'report_menu.dart';
 // void main()
 // {
 //   runApp(const MyApp_1());
@@ -7,102 +7,103 @@ import 'package:flutter/material.dart';
 
 
 // ignore: camel_case_types
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyAPPs extends StatelessWidget {
+  const MyAPPs({super.key});
   
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+    
       home: SafeArea(
         child: Scaffold(
           appBar: AppBar(
-            
           title: const Center(
             child: Text('M-POWER',textAlign: TextAlign.center,
-            style: TextStyle(color: Color.fromARGB(244, 237, 6, 6),fontSize:28),),
+            style: TextStyle(color: Color.fromARGB(244, 254, 252, 252),fontSize:30),),
           ),
           actions: [
             IconButton(onPressed: (){
+              const ReportMenu();
           },
-          icon: const Icon(Icons.notification_add),
+          icon: const Icon(Icons.notification_add,size: 30,),
+
             ),
           ],
-          backgroundColor: const Color.fromARGB(223, 99, 120, 224),
+          backgroundColor: const Color.fromARGB(255, 1, 202, 200),
           ) ,
-          backgroundColor:const Color.fromARGB(223, 254, 243, 67) ,
+          backgroundColor:const Color.fromARGB(235, 206, 255, 252) ,
           drawer: Drawer(
             child: Container(
-            color:const Color.fromARGB(224, 7, 222, 7),
+            color:const Color.fromARGB(255, 1, 202, 200),
               
             child: ListView(
-              children: const <Widget>[
-                    DrawerHeader(
+              children: <Widget>[
+                    const DrawerHeader(
                       child: Center(
-                        child: Text('M-POWER',style: TextStyle(fontSize: 33),
+                        child: Text('M-POWER',style: TextStyle(fontSize: 33,color:  Color.fromARGB(255, 233, 233, 221),),
                         ),
                       ),
                     ),
+
+                    const ListTile(
+                   leading: Icon(Icons.account_circle, size: 30,color: Color.fromARGB(255, 233, 233, 221),),
+                   title: Text('Admin',style: TextStyle(fontSize:20,color: Color.fromARGB(255, 250, 250, 250) ),),
+                    ),
+      
+                    const ListTile(
+                    leading: Icon(Icons.home,size:25,color: Color.fromARGB(255, 200, 236, 33),),
+                    title: Text('Home',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
+                    ),
+
+                  const ListTile(
+                   leading:Icon(Icons.mobile_friendly, size: 25,color: Color.fromARGB(255, 200, 236, 33),),
+                   title: Text('Device',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
+                   hoverColor:Color.fromARGB(255, 234, 241, 241),
+                  ),
+
+                  const ListTile(
+                    leading: Icon(Icons.people,size:25,color: Color.fromARGB(255, 200, 236, 33),),
+                    title: Text('User',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
+                   ),
+                  
+                  
                     
-                   Icon(Icons.account_circle, size: 30),
-                  //  SizedBox(width: 10),
-                   Center(
-                     child: Text(
-                        'Account', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
+                    // DropdownMenu(dropdownMenuEntries: ),
+                    ListTile(
+                    leading:const Icon(Icons.note_alt,color: Color.fromARGB(255, 200, 236, 33),),
+                    title: const Text('Report',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
+                    onTap: () {
+                      Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => const ReportMenu(),)
+                      );
+                    },
+                    ),
+
+                  const ListTile(
+                    
+                    leading: Icon(Icons.subscriptions,color: Color.fromARGB(255, 200, 236, 33),),
+                    title: Text('Subscriptions',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
+                  ),
+
+                   const ListTile( 
+                    leading: Icon(Icons.notifications_active,color: Color.fromARGB(255, 200, 236, 33),),
+                     title: Text('Notification settings',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
                    ),
-                   Icon(Icons.mobile_friendly, size: 30),
-                   Center(
-                     child: Text(
-                        'Device', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
+
+                   const ListTile(
+                   leading:  Icon(Icons.lock_outline_rounded,color: Color.fromARGB(255, 200, 236, 33),),
+                   title:  Text('Change passward',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
                    ),
-                    Icon(Icons.people),
-                    Center(
-                     child: Text(
-                        'User', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
+
+                   const ListTile(
+                    leading: Icon(Icons.logout,color: Color.fromARGB(255, 200, 236, 33),),
+                    title: Text('Logout',style: TextStyle(fontSize: 20,color: Color.fromARGB(255, 250, 250, 250)),),
                    ),
-                    Icon(Icons.note_alt),
-                    Center(
-                     child: Text(
-                        'Report', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
-                   ),
-                   Icon(Icons.subscriptions),
-                   Center(
-                     child: Text(
-                        'Subscription', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
-                   ),
-                   Icon(Icons.notifications_active),
-                   Center(
-                     child: Text(
-                        'Notification settings', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
-                   ),
-                   Icon(Icons.lock_outline_rounded),
-                   Center(
-                     child: Text(
-                        'Change passward', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
-                   ),
-                   Icon(Icons.logout),
-                    Center(
-                     child: Text(
-                        'Logout', // Name of the icon
-                        style: TextStyle(fontSize: 25),
-                       ),
-                   ),
+
               ],
+                   
             ),
           
             )
@@ -110,11 +111,80 @@ class MyApp extends StatelessWidget {
           
           bottomNavigationBar: BottomNavigationBar(items: const [
             BottomNavigationBarItem(label:'home', icon:Icon(Icons.home)),
-            BottomNavigationBarItem(label:'thunder', icon:Icon(Icons.show_chart_outlined)),
-          ],backgroundColor:const Color.fromARGB(122, 456, 234, 34),),
+            BottomNavigationBarItem(label:'thunder', icon:Icon(Icons.flash_on)),
+          ],backgroundColor:const Color.fromARGB(255, 1, 202, 200),),
+          body: Title(color: Colors.white, child: const Text('Dashboard')),
           
          ),
+
       ),
+
       );
 }
 }
+
+// // class ReportMenu extends StatelessWidget {
+// //   const ReportMenu({super.key});
+
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return ListView(
+// //       children: <Widget>[
+// //         ExpansionTile(
+// //           leading: const Icon(
+// //             Icons.note_alt,
+// //             color: Color.fromARGB(255, 200, 236, 33),
+// //           ),
+// //           title: const Text(
+// //             'Report',
+// //             style: TextStyle(fontSize: 20, color: Color.fromARGB(255, 250, 250, 250)),
+// //           ),
+// //           children: <Widget>[
+// //             ListTile(
+// //               leading: const Icon(Icons.assignment, color: Colors.yellow),
+// //               title: const Text('Reading Report'),
+// //               onTap: () {
+// //                 // Handle the tap for Reading Report
+// //               },
+// //             ),
+// //             ListTile(
+// //               leading: const Icon(Icons.pie_chart, color: Colors.yellow),
+// //               title: const Text('Consumption Report'),
+// //               onTap: () {
+// //                 // Handle the tap for Consumption Report
+// //               },
+// //             ),
+// //             ListTile(
+// //               leading: const Icon(Icons.bar_chart, color: Colors.yellow),
+// //               title: const Text('History Report'),
+// //               onTap: () {
+// //                 // Handle the tap for History Report
+// //               },
+// //             ),
+// //             ListTile(
+// //               leading: const Icon(Icons.notifications, color: Colors.yellow),
+// //               title: const Text('Notification Report'),
+// //               onTap: () {
+// //                 // Handle the tap for Notification Report
+// //               },
+// //             ),
+// //             ListTile(
+// //               leading: const Icon(Icons.warning, color: Colors.yellow),
+// //               title: const Text('Alert Report'),
+// //               onTap: () {
+// //                 // Handle the tap for Alert Report
+// //               },
+// //             ),
+// //           ],
+// //         ),
+// //         ListTile(
+// //           leading: const Icon(Icons.subscriptions, color: Colors.yellow),
+// //           title: const Text('Subscription'),
+// //           onTap: () {
+// //             // Handle the tap for Subscription
+// //           },
+// //         ),
+// //       ],
+// //     );
+// //   }
+// // }
