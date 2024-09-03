975d37a01c61d44c16851963e95fcd8b7de41e20
krishnan1235 <133309868+krishnan1235@users.noreply.github.com>
2024-09-03 14:15:02 +0530

    create notify

---
+++ lib/notification.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:power_consumption_analytics/widgets/toggle_card.dart';
+
+class NotiFy extends StatefulWidget {
+  const NotiFy({super.key});
+
+  @override
+  State<NotiFy> createState() => _NotiFyState();
+}
+
+class _NotiFyState extends State<NotiFy> {
+  bool status = false;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color.fromARGB(255, 1, 202, 199),
+          title: const Center(child: Text('Notification Settings')),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                text: const TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '*',
+                      style: TextStyle(
+                        color: Colors.red,
+                        fontSize: 16,
+                      ),
+                    ),
+                    TextSpan(
+                      text:
+                          'Only the selected options trigger on alert notification on your mobile',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      ToggleCard(title: 'High Voltage',),
+                      ToggleCard(title: 'Low Voltage',)
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+                    children: [
+                      ToggleCard(title: 'Poor Power Factor'),
+                      ToggleCard(title: 'Power Failure')
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+
+                    children: [
+                      ToggleCard(title: 'Communication Fail',),
+                      ToggleCard(title: 'Negetive Consumed Value',)
+                    ],
+                  )
+                ],
+              )
+            ],
+          ),
+        ),
+
+        //  Text("Only the selected options triggers an alert notification on your mobile"),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/toggle_card.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class ToggleCard extends StatefulWidget {
+  final String title;
+  // final Function func;
+  const ToggleCard({
+    super.key,
+    required,
+    required this.title,
+  });
+  @override
+  State<ToggleCard> createState() => _ToggleCardState();
+}
+
+class _ToggleCardState extends State<ToggleCard> {
+  bool status = false;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        width: 174,
+        height: 120,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            // color: status?Colors.green:Colors.red,
+            color: Colors.white,
+            // border: Border.all(
+            // color: const Color.fromARGB(255, 212, 209, 209),
+            //   style: BorderStyle.solid,
+            // ),
+            borderRadius: BorderRadius.circular(11),
+            boxShadow: const [
+              BoxShadow(
+                color: Color.fromARGB(255, 242, 238, 238),
+                offset: Offset(-1.0,-1.0),
+                blurRadius: 1,
+                spreadRadius: 1.0,
+              ),
+            ]),
+        child: Column(
+          children: [
+            Text(widget.title),
+            const SizedBox(
+              height: 1.0,
+              width: 4.0,
+            ),
+        Padding(
+  padding: const EdgeInsets.all(1.8),
+  child: Stack(
+    alignment: Alignment.center,
+    children: [
+      Switch(
+        inactiveThumbColor: Colors.white,
+        inactiveTrackColor: Colors.red,
+        activeTrackColor: const Color.fromARGB(214, 10, 246, 69),
+        value: status,
+        onChanged: (value) {
+          setState(() {
+            status = value;
+          });
+        },
+      ),
+      Positioned(
+        left: status ? 10.10 : 30.0, // Adjust based on the thumb's position
+        child: Text(
+          status ? 'ON' : 'OFF',
+          style: const TextStyle(
+            color: Color.fromARGB(255, 244, 241, 241),
+            fontSize: 11,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    ],
+  ),
+),
+
+          ],
+        ));
+  }
+}
